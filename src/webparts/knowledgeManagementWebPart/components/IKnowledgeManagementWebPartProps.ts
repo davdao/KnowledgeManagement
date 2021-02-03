276712973cd28791ab2cf8b468de36f31ff5210f
DAVID DAO <dav.dao@outlook.com>
2021-02-03 19:09:40 +0100

    Create project

---
+++ src/webparts/knowledgeManagementWebPart/components/IKnowledgeManagementWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface IKnowledgeManagementWebPartProps {
+  description: string;
+}
---
+++ src/webparts/knowledgeManagementWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IKnowledgeManagementWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'KnowledgeManagementWebPartWebPartStrings' {
+  const strings: IKnowledgeManagementWebPartWebPartStrings;
+  export = strings;
+}
